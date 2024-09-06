374b5c16734413541995e3105a10614020c15c70
paolaperdomo <paolaperdomo079@gmail.com>
2024-09-06 11:26:47 +0200

    first commit

---
+++ src/main/java/com/film_radar/film_radar_backend/FilmRadarBackendApplication.java
@@ -0,0 +1,14 @@
+package com.film_radar.film_radar_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmRadarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmRadarBackendApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/test/java/com/film_radar/film_radar_backend/FilmRadarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.film_radar.film_radar_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmRadarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
